3092f3132b68b7f5d65cd97aad0838e89cb90ee6
승진 <92sharp@naver.com>
2022-08-10 20:37:18 +0900

    문자열 : 상수

---
+++ baekjoon/step06_문자열/problem2908_상수.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	string str1, str2;
+
+	cin >> str1 >> str2;
+	cin.get();
+
+	reverse(str1.begin(), str1.end());
+	reverse(str2.begin(), str2.end());
+
+	if (stoi(str1) > stoi(str2)) {
+		cout << stoi(str1);
+	}
+	else {
+		cout << stoi(str2);
+	}
+
+	return 0;
+}
